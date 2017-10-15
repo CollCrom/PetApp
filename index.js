b40d6e485d42ea00b44f93d06621dee4df5ee689
Collin Cromwell <collcrom@gmail.com>
2017-10-14 19:14:28 -0500

    sever is up

---
+++ index.js
@@ -0,0 +1,18 @@
+//require('./db/db');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+//const petController = require('./controllers/pets')
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+//app.use('/', petController);
+app.listen(5050, () =>{
+	console.log('server is up');
+})
