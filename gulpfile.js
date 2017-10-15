2b63f1a4baf192e1524d621003c112e806b94391
Collin Cromwell <collcrom@gmail.com>
2017-10-14 22:02:26 -0500

    styled the page with less

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 
 gulp.task('less-css', () =>{
-	gulp.src('/*.less')
+	gulp.src('./public/styles/*.less')
 		.pipe(less())
-		.pipe(gulp.dest('/'));
+		.pipe(gulp.dest('./public/styles/'));
 });
 
 gulp.task('watch', () =>{
